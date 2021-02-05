6e608836528789a833215cee946f1f8734ac04d0
siccoo <obichilaka@gmail.com>
2021-02-05 17:30:26 +0100

    Updates on App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      
+        <Router>
+          <Switch>
+            
+          </Switch>
+        </Router>
     </div>
   );
 }
