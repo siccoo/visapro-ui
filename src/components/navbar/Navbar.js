53d7ab2986a5d8020cc89c449058e05a01295907
siccoo <obichilaka@gmail.com>
2021-02-05 17:23:58 +0100

    Setting up components, images & pages folder

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+const Topbar = () => {
+    return (
+        <div>
+            This is Navbar view
+        </div>
+    )
+}
+
+export default Topbar;
