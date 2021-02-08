2060f6b22211510fe7af8a63a8f1463bb74a31f6
siccoo <obichilaka@gmail.com>
2021-02-08 15:45:28 +0100

    updates on Navbar.js, Navbar.css & App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 
+import Topbar from "./components/navbar/Navbar";
+
 const App = () => {
   return (
     <div className="App">
         <Router>
+          <Topbar />
           <Switch>
 
           </Switch>
