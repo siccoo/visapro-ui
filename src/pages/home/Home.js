4954c07fe6ff4ab4154efc117c8aacc443e5a262
siccoo <obichilaka@gmail.com>
2021-02-08 19:21:55 +0100

    updates on Home.js

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -16,7 +16,29 @@ const Home = () => {
                         <div className="col-md-12 text-center">
                             <h1>The world is yours to explore</h1>
                             <div className="gray-border">
-
+                                <form>
+                                    <div className="form-row align-self-center">
+                                        <div className="col-auto my-1">
+                                            <select className=" mr-sm-2" id="">
+                                                <option selected>Choose...</option>
+                                                <option value="1">One</option>
+                                                <option value="2">Two</option>
+                                                <option value="3">Three</option>
+                                            </select>
+                                        </div>
+                                        <div className="col-auto my-1">
+                                            <select className=" mr-sm-2" id="">
+                                                <option selected>Choose...</option>
+                                                <option value="1">One</option>
+                                                <option value="2">Two</option>
+                                                <option value="3">Three</option>
+                                            </select>
+                                        </div>
+                                        <div className="col-auto my-1">
+                                            <button type="submit" className="btn-header">Submit</button>
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
