62bed2df2a2f7698783cb8229a32568eb6cab7e4
siccoo <obichilaka@gmail.com>
2021-02-08 17:04:33 +0100

    updates on Home.js & index.html

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import Navbar from "../../components/navbar/Navbar";
 import "./Home.css";
 
 const Home = () => {
@@ -8,7 +9,10 @@ const Home = () => {
     }, []);
     return (
         <div>
-            
+            <section className="header">
+                <Navbar />
+                
+            </section>
         </div>
     )
 }
