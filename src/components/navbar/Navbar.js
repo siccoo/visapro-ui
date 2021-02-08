44f0b9e72d106760624b8ea603bfb464239ae8b6
siccoo <obichilaka@gmail.com>
2021-02-08 15:20:38 +0100

    updates on Navbar.js section

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,61 @@
 import React from "react";
 import "./Navbar.css";
 
+import VisaproLogo from "../../images/VisaproLogo.svg";
+
 const Topbar = () => {
     return (
         <div>
-            This is Navbar view
+            <section className="nav-section">
+            <div className="container-fluid">
+                <nav className="navbar navbar-expand-lg navbar-light nav-bg">
+                <a className="navbar-brand" href="/">
+                    <img src={VisaproLogo} className="home-logo" alt="home-logo" />
+                </a>
+                <button className="navbar-toggler ml-auto hidden-sm-up float-xs-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="line"></span> 
+                    <span className="line"></span> 
+                    <span className="line"></span>
+                </button>
+                <div id="navbarNavDropdown" className="navbar-collapse collapse">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item">
+                            <NavLink
+                                className="nav-link"
+                                to="/"
+                            >
+                            Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                className="nav-link"
+                                to="/contact"
+                            >
+                            Contact us
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                className="nav-link"
+                                to="/faq"
+                            >
+                            FAQ's
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink
+                                className="nav-btn"
+                                to="/user"
+                            >
+                            Login
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+                </nav>
+            </div>
+        </section>
         </div>
     )
 }
