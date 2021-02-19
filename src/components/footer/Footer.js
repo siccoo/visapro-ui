594935f2798134134758d1e82ee0187dcf8f97a6
siccoo <obichilaka@gmail.com>
2021-02-19 17:42:09 +0100

    updates on Footer.js

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "Footer.css";
+
+const Footer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Footer;
