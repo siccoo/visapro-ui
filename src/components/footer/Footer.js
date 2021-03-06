43ca362a789c0665dd910787208a0629575bfb8d
siccoo <obichilaka@gmail.com>
2021-03-06 19:44:34 +0100

    Structuring the blocks of Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,32 +6,35 @@ const Footer = () => {
     return (
         <div>
             <section className="footer-section">
-                <div className="col-md-1"></div>
-                <div className="col-md-1"></div>  
-                <div className="col-md-3">
-                    <ul className="unorder-list">
-                        <li className="list-item">
-                            <Link to="/contact" className="list-head">Contact us</Link>
-                        </li>
-                        <li className="list-item">
-                            <Link to="/about" className="list-head">About us</Link>
-                        </li>
-                        <li className="list-item">
-                            <Link to="/mission" className="list-head">Our mission</Link>
-                        </li>
-                    </ul>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-1"></div>
+                        <div className="col-md-1"></div>  
+                        <div className="col-md-3">
+                            <ul className="unorder-list">
+                                <li className="list-item">
+                                    <Link to="/contact" className="list-head">Contact us</Link>
+                                </li>
+                                <li className="list-item">
+                                    <Link to="/about" className="list-head">About us</Link>
+                                </li>
+                                <li className="list-item">
+                                    <Link to="/mission" className="list-head">Our mission</Link>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className="col-md-3">
+                            <ul className="unorder-list">
+                                <li className="list-item">
+                                    <Link to="/address" className="list-head">Contact us</Link>
+                                </li>
+                            </ul>
+                            <p>3, oladele close off makarios street,
+                                Ajah, Lagos,
+                                Nigeria</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-md-3">
-                    <ul className="unorder-list">
-                        <li className="list-item">
-                            <Link to="/address" className="list-head">Contact us</Link>
-                        </li>
-                    </ul>
-                    <p>3, oladele close off makarios street,
-                        Ajah, Lagos,
-                        Nigeria</p>
-                </div>
-                
             </section>
         </div>
     )
