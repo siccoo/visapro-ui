104be69714040515af536c8986a1855df1f8547d
siccoo <obichilaka@gmail.com>
2021-03-06 19:35:08 +0100

    Structuring the blocks of Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,18 +11,22 @@ const Footer = () => {
                 <div className="col-md-3">
                     <ul className="unorder-list">
                         <li className="list-item">
-                            <Link to="/about" className="list-head">About Us</Link>
+                            <Link to="/contact" className="list-head">Contact us</Link>
                         </li>
                         <li className="list-item">
-                            <Link to="/about" className="list-head">About Us</Link>
+                            <Link to="/about" className="list-head">About us</Link>
                         </li>
                         <li className="list-item">
-                            <Link to="/about" className="list-head">About Us</Link>
+                            <Link to="/mission" className="list-head">Our mission</Link>
                         </li>
                     </ul>
                 </div>
                 <div className="col-md-3">
-
+                    <ul className="unorder-list">
+                        <li className="list-item">
+                            <Link to="/address" className="list-head">Contact us</Link>
+                        </li>
+                    </ul>
                 </div>
             </section>
         </div>
